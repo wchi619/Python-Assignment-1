217b03eb9a37f471681fa38d962fa908b49cd585
William Chi <54991733+wchi619@users.noreply.github.com>
2019-10-06 14:26:50 -0400

    Create checkA1.py

---
+++ checkA1.py
@@ -0,0 +1,30 @@
+[wchi@c7-wchi as1]$ ./checkA1.py 
+Please enter your email user id:wchi3
+
+== Preliminary A1 Test Run Report for wchi3==
+The following is your preliminary test run report for assignment 1. Please review the report and fix all the errors identified before submitting your algorithm, python script, and test report to blackboard using the assignment 1 submission link which will be available on Monday, October 7, 2019.
+
+========================================
+Test run command 1 : python3 a1_wchi3.py 2018/01/01 1
+--test passed--
+Test run command 2 : python3 a1_wchi3.py 2018/01/01 -1
+--test passed--
+Test run command 3 : python3 a1_wchi3.py 2018/01/01 2
+--test passed--
+Test run command 4 : python3 a1_wchi3.py --step 2018/01/01 3
+--test passed--
+Test run command 5 : python3 a1_wchi3.py 2018/07/01 500
+--test passed--
+Test run command 6 : python3 a1_wchi3.py 2018/99/01 2
+--test passed--
+Test run command 7 : python3 a1_wchi3.py 2018/01/99 2
+--test passed--
+Test run command 8 : python3 a1_wchi3.py 2018 2
+--test passed--
+Test run command 9 : python3 a1_wchi3.py 2020/02/28 1
+--test passed--
+Test run command 10 : python3 a1_wchi3.py 2020/03/01 -1
+--test passed--
+Test Results: {1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1, 10: 1}
+Total test run marks:  48.0
+Total marks for script (max. 48): 48.0
